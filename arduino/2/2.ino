bd7faba1e4def2f361d596a28d745beed8b9e218
hujialiang <1020120306@qq.com>
2021-01-21 20:49:54 +0800

    开关灯

--- arduino/2/2.ino
+++ arduino/2/2.ino
@@ -5,6 +5,11 @@ int buttonOn = 0;
 
 void setup() {  
   pinMode(12, OUTPUT);  
+  pinMode(11, OUTPUT); 
+  pinMode(10, OUTPUT);
+  pinMode(9, OUTPUT);
+  pinMode(8, OUTPUT);
+  pinMode(6, OUTPUT);
   pinMode(7, INPUT);
 }
 
@@ -12,7 +17,20 @@ void loop() {
   buttonOn = digitalRead(7);  
   if (buttonOn == HIGH) {    
     digitalWrite(12, HIGH);
-  } else {    
+    digitalWrite(11, HIGH);
+    digitalWrite(10, HIGH);
+    digitalWrite(9, HIGH);
+    digitalWrite(8, HIGH);
+    digitalWrite(6, HIGH);
+}    
+
+  
+  else {    
     digitalWrite(12, LOW);
+    digitalWrite(11, LOW);
+    digitalWrite(10, LOW);
+    digitalWrite(9, LOW);
+    digitalWrite(8, LOW);
+    digitalWrite(6, LOW);
   }
 }
